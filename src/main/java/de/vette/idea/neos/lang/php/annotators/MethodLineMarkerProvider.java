ba665f94facab7a3d2bedf4c70ce1a9c01e1cf38
cvette <vette.christian@gmail.com>
2017-09-07 20:36:23 +0200

    FEATURE: Implement line marker for controller actions
    This makes it possible to jump to a controller action template.

---
+++ src/main/java/de/vette/idea/neos/lang/php/annotators/MethodLineMarkerProvider.java
@@ -0,0 +1,62 @@
+/*
+ *  IntelliJ IDEA plugin to support the Neos CMS.
+ *  Copyright (C) 2016  Christian Vette
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.vette.idea.neos.lang.php.annotators;
+
+import com.intellij.codeInsight.daemon.LineMarkerInfo;
+import com.intellij.codeInsight.daemon.LineMarkerProvider;
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
+import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.jetbrains.php.lang.psi.elements.Method;
+import de.vette.idea.neos.NeosIcons;
+import de.vette.idea.neos.lang.fusion.resolve.ResolveEngine;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.List;
+
+public class MethodLineMarkerProvider implements LineMarkerProvider {
+
+    @Nullable
+    @Override
+    public LineMarkerInfo getLineMarkerInfo(@NotNull PsiElement element) {
+        return null;
+    }
+
+    @Override
+    public void collectSlowLineMarkers(@NotNull List<PsiElement> elements, @NotNull Collection<LineMarkerInfo> result) {
+        for (PsiElement el : elements) {
+            if (el instanceof Method && ((Method) el).getAccess().isPublic()) {
+                VirtualFile template = ResolveEngine.findTemplate((Method) el);
+                if (template != null) {
+                    PsiFile target = PsiManager.getInstance(el.getProject()).findFile(template);
+                    RelatedItemLineMarkerInfo info = NavigationGutterIconBuilder
+                            .create(NeosIcons.NODE_TYPE)
+                            .setTarget(target)
+                            .setTooltipText("Go to template")
+                            .createLineMarkerInfo(el);
+                    result.add(info);
+                }
+            }
+        }
+    }
+}
