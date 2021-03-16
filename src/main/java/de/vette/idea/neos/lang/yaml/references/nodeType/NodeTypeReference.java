1f4fd41cb1a747926e2ba30facfecd7b1b7c4b86
Matthias Klatte <matthias.klatte@gmail.com>
2021-03-16 17:53:50 +0100

    FEATURE: generate node-type references for other occurrences
    it should be possible to find references in
    * childNodes.type (scalar value)
    * editorOptions.nodeTypes (array value)
    
    TODO
    ```
    editorOptions:
      nodeTypes:
        - Some.Package:Example1
        - 'Some.Package:Example2'
        -
          'Some.Package.Example3'
    ```
    will currently not create the correct text-range

--- src/main/java/de/vette/idea/neos/lang/yaml/references/nodeType/NodeTypeReference.java
+++ src/main/java/de/vette/idea/neos/lang/yaml/references/nodeType/NodeTypeReference.java
@@ -11,20 +11,58 @@ import com.intellij.util.indexing.FileBasedIndex;
 import de.vette.idea.neos.indexes.NodeTypesYamlFileIndex;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.yaml.YAMLUtil;
-import org.jetbrains.yaml.psi.YAMLFile;
-import org.jetbrains.yaml.psi.YAMLKeyValue;
+import org.jetbrains.yaml.psi.*;
+
 import java.util.Collection;
 
 /**
  * Resolving the Node Type when it is clicked
  */
-public class NodeTypeReference extends PsiPolyVariantReferenceBase<YAMLKeyValue> {
-    private final YAMLKeyValue yamlElement;
+public class NodeTypeReference extends PsiPolyVariantReferenceBase<YAMLPsiElement> {
+    private final YAMLPsiElement yamlElement;
+    private final String nodeTypeName;
+
+    private static TextRange textRangeFromElement(YAMLPsiElement yamlElement, boolean isKey) {
+        int sourceStart = 0;
+        int sourceEnd = yamlElement.getTextLength();
+        if (isKey && yamlElement instanceof YAMLKeyValue) {
+            sourceEnd = ((YAMLKeyValue) yamlElement).getKey().getTextLength();
+        } else if (yamlElement instanceof YAMLKeyValue) {
+            sourceStart = ((YAMLKeyValue) yamlElement).getValue().getStartOffsetInParent();
+            sourceEnd = ((YAMLKeyValue) yamlElement).getValue().getTextLength();
+        } else if (yamlElement instanceof YAMLSequenceItem) {
+            sourceStart = ((YAMLSequenceItem) yamlElement).getValue().getStartOffsetInParent();
+            sourceEnd = ((YAMLSequenceItem) yamlElement).getValue().getTextLength();
+        } else {
+            return yamlElement.getTextRange();
+        }
+        return new TextRange(sourceStart, sourceStart + sourceEnd);
+    }
+
+    public NodeTypeReference(YAMLKeyValue yamlElement, boolean isKey) {
+        // the "textRange" is used for highlighting the source element
+        super(yamlElement, textRangeFromElement(yamlElement, isKey));
+        this.yamlElement = yamlElement;
+        if (isKey) {
+            this.nodeTypeName = yamlElement.getKeyText();
+        } else {
+            this.nodeTypeName = yamlElement.getValueText();
+        }
+    }
 
     NodeTypeReference(YAMLKeyValue yamlElement) {
-        // the "textRange" is used for highlighting the source element
-        super(yamlElement, new TextRange(0, yamlElement.getKey().getTextLength() - 1));
+        this(yamlElement, true);
+    }
+
+    NodeTypeReference(YAMLSequenceItem yamlElement) {
+        super(yamlElement.getValue() == null ? yamlElement : yamlElement.getValue(), textRangeFromElement(yamlElement, false));
         this.yamlElement = yamlElement;
+        var textValue = yamlElement.getValue().getText();
+        if (yamlElement.getValue() instanceof YAMLQuotedText) {
+            this.nodeTypeName = textValue.substring(1, textValue.length() - 1);
+        } else {
+            this.nodeTypeName = textValue;
+        }
     }
 
     @NotNull
@@ -36,10 +74,9 @@ public class NodeTypeReference extends PsiPolyVariantReferenceBase<YAMLKeyValue>
     @NotNull
     @Override
     public ResolveResult[] multiResolve(boolean incompleteCode) {
-        String nodeTypeNameToFindReferenceFor = yamlElement.getKeyText();
 
         // files which contain the NodeType definition
-        Collection<VirtualFile> files = FileBasedIndex.getInstance().getContainingFiles(NodeTypesYamlFileIndex.KEY, nodeTypeNameToFindReferenceFor, GlobalSearchScope.allScope(yamlElement.getProject()));
+        Collection<VirtualFile> files = FileBasedIndex.getInstance().getContainingFiles(NodeTypesYamlFileIndex.KEY, nodeTypeName, GlobalSearchScope.allScope(yamlElement.getProject()));
 
         return files
                 .stream()
@@ -51,7 +88,7 @@ public class NodeTypeReference extends PsiPolyVariantReferenceBase<YAMLKeyValue>
                 // get all YAML keys in these files
                 .flatMap(yamlFile -> YAMLUtil.getTopLevelKeys(yamlFile).stream())
                 // get the correct YAML key
-                .filter(yamlKeyValue -> yamlKeyValue.getKeyText().equals(nodeTypeNameToFindReferenceFor))
+                .filter(yamlKeyValue -> yamlKeyValue.getKeyText().equals(nodeTypeName))
                 // remove "current" element if it exists
                 .filter(yamlKeyValue -> yamlElement != yamlKeyValue)
                 // build up the result object
--- src/main/java/de/vette/idea/neos/lang/yaml/references/nodeType/NodeTypeReferenceContributor.java
+++ src/main/java/de/vette/idea/neos/lang/yaml/references/nodeType/NodeTypeReferenceContributor.java
@@ -11,6 +11,9 @@ import com.intellij.util.ProcessingContext;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.yaml.psi.YAMLKeyValue;
+import org.jetbrains.yaml.psi.YAMLPsiElement;
+import org.jetbrains.yaml.psi.YAMLScalar;
+import org.jetbrains.yaml.psi.YAMLSequenceItem;
 
 /**
  * Entry point for "go to definition" "cmd-click" on a node type name in NodeTypes.yaml to jump to its definition.
@@ -20,11 +23,52 @@ public class NodeTypeReferenceContributor extends PsiReferenceContributor {
     public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
         registrar.registerReferenceProvider(
                 PlatformPatterns.psiElement(YAMLKeyValue.class),
-                new NodeTypeReferenceProvider()
+                new KeyValueNodeTypeReferenceProvider()
+        );
+        registrar.registerReferenceProvider(
+                PlatformPatterns.psiElement(YAMLScalar.class)
+                    .withAncestor(1, PlatformPatterns.psiElement(YAMLSequenceItem.class)),
+                new SequenceItemNodeTypeReferenceProvider()
         );
     }
 
-    private static class NodeTypeReferenceProvider extends PsiReferenceProvider {
+    private static abstract class AbstractNodeTypeReferenceProvider extends PsiReferenceProvider {
+
+        /**
+         * Helper functions to walk the yaml tree
+         */
+        protected static boolean parentKeyIs(YAMLKeyValue yamlKeyValue, String parentKey) {
+            YAMLKeyValue key = getParentKey(yamlKeyValue);
+            return (key != null && parentKey.equals(key.getKeyText()));
+        }
+
+        protected static boolean grandparentKeyIs(YAMLKeyValue yamlKeyValue, String grandparentKey) {
+            YAMLKeyValue key = getParentKey(yamlKeyValue);
+            if (key != null) {
+                key = getParentKey(key);
+                return (key != null && grandparentKey.equals(key.getKeyText()));
+            }
+            return false;
+        }
+
+        protected static boolean isOnRootLevel(YAMLKeyValue yamlKeyValue) {
+            return getParentKey(yamlKeyValue) == null;
+        }
+
+        protected static <T extends YAMLPsiElement> T getParent(YAMLPsiElement yamlElement, Class<T> type) {
+            return PsiTreeUtil.getParentOfType(yamlElement, type);
+        }
+
+        protected static YAMLKeyValue getParentKey(YAMLPsiElement yamlElement) {
+            return getParent(yamlElement, YAMLKeyValue.class);
+        }
+    }
+
+
+    /**
+     * Provide references based on key-value entries, i.e. node-types as configuration keys or simple values
+     */
+    private static class KeyValueNodeTypeReferenceProvider extends AbstractNodeTypeReferenceProvider {
         @NotNull
         @Override
         public PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
@@ -35,39 +79,44 @@ public class NodeTypeReferenceContributor extends PsiReferenceContributor {
             // - constraints.nodeTypes
             // - root level (to find other definitions on root)
             if (parentKeyIs(yamlElement, "superTypes")
-                    || (parentKeyIs(yamlElement, "nodeTypes") && grandparentKeyIs(yamlElement, "constraints"))
-                    || isOnRootLevel(yamlElement)) {
+                || (parentKeyIs(yamlElement, "nodeTypes") && grandparentKeyIs(yamlElement, "constraints"))
+                || isOnRootLevel(yamlElement)) {
                 return new PsiReference[]{
-                        new NodeTypeReference(yamlElement)
+                    new NodeTypeReference(yamlElement)
+                };
+            }
+
+            // we also support:
+            // - childNodes.type (works for node-templates as well)
+            if (yamlElement.getKeyText().equals("type") && grandparentKeyIs(yamlElement, "childNodes")) {
+                return new PsiReference[]{
+                    new NodeTypeReference(yamlElement, false)
                 };
             }
 
             return PsiReference.EMPTY_ARRAY;
         }
+    }
 
-        /**
-         * Helper functions to walk the yaml tree
-         */
-        private static boolean parentKeyIs(YAMLKeyValue yamlKeyValue, String parentKey) {
-            YAMLKeyValue key = getParentKey(yamlKeyValue);
-            return (key != null && parentKey.equals(key.getKeyText()));
-        }
+    /**
+     * Provide references based on string lists, i.e. allowed node-types in editor-options
+     */
+    private static class SequenceItemNodeTypeReferenceProvider extends AbstractNodeTypeReferenceProvider {
+        @NotNull
+        @Override
+        public PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+            YAMLSequenceItem yamlItem = getParent((YAMLPsiElement) element, YAMLSequenceItem.class);
+            YAMLKeyValue yamlElement = getParentKey(yamlItem);
 
-        private static boolean grandparentKeyIs(YAMLKeyValue yamlKeyValue, String grandparentKey) {
-            YAMLKeyValue key = getParentKey(yamlKeyValue);
-            if (key != null) {
-                key = getParentKey(key);
-                return (key != null && grandparentKey.equals(key.getKeyText()));
+            // we support
+            // - editorOptions.nodeTypes
+            if (yamlElement.getKeyText().equals("nodeTypes") && parentKeyIs(yamlElement, "editorOptions")) {
+                return new PsiReference[]{
+                    new NodeTypeReference(yamlItem)
+                };
             }
-            return false;
-        }
 
-        private static boolean isOnRootLevel(YAMLKeyValue yamlKeyValue) {
-            return getParentKey(yamlKeyValue) == null;
-        }
-
-        private static YAMLKeyValue getParentKey(YAMLKeyValue yamlKeyValue) {
-            return PsiTreeUtil.getParentOfType(yamlKeyValue, YAMLKeyValue.class);
+            return PsiReference.EMPTY_ARRAY;
         }
     }
 }
