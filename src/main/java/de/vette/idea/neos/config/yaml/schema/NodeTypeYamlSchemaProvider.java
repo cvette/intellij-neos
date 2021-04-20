a59af072b2d8e2cd43d8e087969f4d2b0c8d5bd4
Christian Vette <github@christianvette.de>
2021-04-20 19:48:24 +0200

    FEATURE: Add provider for Node Type JSON schema

---
+++ src/main/java/de/vette/idea/neos/config/yaml/schema/NodeTypeYamlSchemaProvider.java
@@ -0,0 +1,53 @@
+package de.vette.idea.neos.config.yaml.schema;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.jetbrains.jsonSchema.extension.JsonSchemaFileProvider;
+import com.jetbrains.jsonSchema.extension.SchemaType;
+import com.jetbrains.jsonSchema.impl.JsonSchemaVersion;
+import com.jetbrains.jsonSchema.remote.JsonFileResolver;
+import de.vette.idea.neos.NeosProjectService;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class NodeTypeYamlSchemaProvider implements JsonSchemaFileProvider {
+    protected Project project;
+
+
+    public NodeTypeYamlSchemaProvider(Project project) {
+        this.project = project;
+    }
+
+    @Override
+    public boolean isAvailable(@NotNull VirtualFile file) {
+        return !project.isDisposed() && NeosProjectService.isEnabled(project) && file.getName().startsWith("NodeTypes.");
+    }
+
+    @Override
+    public @NotNull @Nls String getName() {
+        return "Neos Node Type";
+    }
+
+    @Override
+    public @Nullable VirtualFile getSchemaFile() {
+        return JsonFileResolver.urlToFile(this.getRemoteSource());
+    }
+
+    @Override
+    public @NotNull SchemaType getSchemaType() {
+        return SchemaType.remoteSchema;
+    }
+
+    @Override
+    public JsonSchemaVersion getSchemaVersion() {
+        return JsonSchemaVersion.SCHEMA_7;
+    }
+
+    @Override
+    public @Nullable
+    @NonNls String getRemoteSource() {
+        return "https://raw.githubusercontent.com/Sebobo/Shel.Neos.Schema/main/NodeTypes.Schema.json";
+    }
+}
---
+++ src/main/java/de/vette/idea/neos/config/yaml/schema/NodeTypeYamlSchemaProviderFactory.java
@@ -0,0 +1,16 @@
+package de.vette.idea.neos.config.yaml.schema;
+
+import com.intellij.openapi.project.Project;
+import com.jetbrains.jsonSchema.extension.JsonSchemaFileProvider;
+import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class NodeTypeYamlSchemaProviderFactory implements JsonSchemaProviderFactory {
+
+    @Override
+    public @NotNull List<JsonSchemaFileProvider> getProviders(@NotNull Project project) {
+        return List.of(new NodeTypeYamlSchemaProvider(project));
+    }
+}
