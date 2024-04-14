9329599203b013235aaf35dfcb17e0156a875153
Christian Vette <6884391+cvette@users.noreply.github.com>
2024-04-14 20:31:08 +0200

    fix: Gradle - Removed Qodana Gradle Plugin configuration to rely on defaults

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,14 +60,6 @@ changelog {
     repositoryUrl = properties("pluginRepositoryUrl")
 }
 
-// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
-qodana {
-    cachePath = provider { file(".qodana").canonicalPath }
-    reportPath = provider { file("build/reports/inspections").canonicalPath }
-    saveReport = true
-    showReport = environment("QODANA_SHOW_REPORT").map { it.toBoolean() }.getOrElse(false)
-}
-
 grammarKit {
     jflexRelease = properties("jFlexVersion")
     grammarKitRelease = properties("grammarKitVersion")
