571ffcb2992ac2a05f23863202c8ce0c368bc693
Matthias Klatte <matthias.klatte@gmail.com>
2024-09-14 20:05:53 +0200

    feat: implement intention to split fusion path at caret

---
+++ src/test/java/de/vette/idea/neos/fusion/codeInsight/intention/FusionPathIntentionTest.java
@@ -0,0 +1,28 @@
+package de.vette.idea.neos.fusion.codeInsight.intention;
+
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import util.FusionTestUtils;
+
+import java.io.File;
+
+public class FusionPathIntentionTest extends BasePlatformTestCase {
+    @Override
+    protected @NonNls @NotNull String getTestDataPath() {
+        return FusionTestUtils.BASE_TEST_DATA_PATH + File.separator + "fusion" + File.separator + "codeInsight" + File.separator + "intention";
+    }
+
+    public void testSplitFusionPath() {
+        doTest("splitFusionPath", "Split fusion path");
+    }
+
+    private void doTest(String testName, String hint) {
+        myFixture.configureByFile(testName + ".before.fusion");
+        final IntentionAction action = myFixture.findSingleIntention(hint);
+        assertNotNull(action);
+        myFixture.launchAction(action);
+        myFixture.checkResultByFile(testName + ".after.fusion");
+    }
+}
